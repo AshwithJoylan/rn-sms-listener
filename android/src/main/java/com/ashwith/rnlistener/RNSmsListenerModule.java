ad16d2772f896972ed5214131637150815eb55fd
AshwithJoylan <ashwith00@gmail.com>
2020-06-15 12:08:13 +0530

    version 1.0.2

--- android/src/main/java/com/ashwith/rnlistener/RNSmsListenerModule.java
+++ android/src/main/java/com/ashwith/rnlistener/RNSmsListenerModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.ashwith.rnlistener;
 
 import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
--- android/src/main/java/com/ashwith/rnlistener/SmsReceiver.java
+++ android/src/main/java/com/ashwith/rnlistener/SmsReceiver.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.ashwith.rnlistener;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
